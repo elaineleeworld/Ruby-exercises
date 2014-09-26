8e7c3da663d03e876c77235172d205e233863143
Elaine Lee <ecl6@cornell.edu>
2014-09-25 22:14:00 -0700

    Update two_sum.rb

--- two_sum.rb
+++ two_sum.rb
@@ -1,18 +1,15 @@
 
 # return true if an array has two numbers that sum to 0
-# must have 2 zeroes!
+# else false!
 
-def two_sum(nums)
-        answer = "true"
-        num_of_zeroes = 0
+def zero_sum_pair(nums)
         
-        nums.each do |num|
-        if num == 0
-            num_of_zeroes += 1
+        nums.size.times do |i|
+                (i+1..nums.length-1).each do |j|
+                        return true if nums[i] + nums[j] == 0
+                end
         end
-        if num_of_zeroes == 2
-            return answer
-        end
-    end
-    return "false"
+        false
 end
+zero_sum_pair([1,-1])   #=> true
+zero_sum_pair([0,10,5])  # => false
