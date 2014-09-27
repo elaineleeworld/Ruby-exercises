74e47c5159a7f1ff07d2f8e95d9c535eb28ac6ec
Elaine Lee <ecl6@cornell.edu>
2014-09-26 23:23:00 -0700

    Update ordered_vowel_word.rb

--- ordered_vowel_word.rb
+++ ordered_vowel_word.rb
@@ -1,5 +1,11 @@
 def ordered_vowel_word(str)
-
+    result = ( )
+    
+    str.split(/ /).each do |i|
+        result << i if ordered_vowel_word?(i)
+    end
+    p result.join(" ")
+end
 
 
 
@@ -17,4 +23,6 @@ def ordered_vowel_word?(word)
     end
 end
 
-ordered_vowel_word?("hello")
+#ordered_vowel_word?("hello")
+
+ordered_vowel_word("this is a test of the vowel ordering system")
