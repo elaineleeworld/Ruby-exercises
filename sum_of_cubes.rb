5328e3862b2e62b2b7f559fbf3aaecfe28d9d207
Elaine Lee <ecl6@cornell.edu>
2014-09-28 20:35:57 -0700

    Update sum_of_cubes.rb

--- sum_of_cubes.rb
+++ sum_of_cubes.rb
@@ -1,12 +1,14 @@
-def sum_of_cubes(num)
+# sum of cubes from a number (a) to another number (b)
+
+def sum_of_cubes(a,b)
      
     sum = 0
      
-     (1..num).each {|i| sum += (i**3)}
+     (a..b).each {|i| sum += (i**3)}
        
      p sum
  end
     
-sum_of_cubes(4)    
+sum_of_cubes(3,5)  # => 216  
         
 # sum = 1^3 + 2^3.. + num^3
