b08025f1b0d2c142ea7df7b6dbb5a7a07ee836cb
Elaine Lee <ecl6@cornell.edu>
2014-09-29 01:49:25 -0700

    Update my_uniq.rb

--- my_uniq.rb
+++ my_uniq.rb
@@ -1,11 +1,10 @@
 def my_uniq(inputs)
         results = [ ]
         
-        inputs.each do |input|
-        results << input unless results.include?(input)
+        inputs.select {|i| results << i if inputs.count(i) == 1}
         end
-            results
+         p  results
 end
 
-puts my_uniq([3,2,1,3,4,4,5])
+puts my_uniq([3,2,1,3,4,4,5]) # => [2,1,5]
 
